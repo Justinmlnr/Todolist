5a88deb50e3d388e4992bf6fe22da000829a5b35
Justinmlnr <juju.vlb38@gmail.com>
2026-04-07 11:39:05 +0200

    Todolist & Users

--- Todo.py
+++ Todo.py
@@ -4,3 +4,4 @@ class Todo(BaseModel):
     id: int | None = None
     title: str
     completed: bool = False
+    user_id: int
--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from fastapi import FastAPI, HTTPException
 from Todo import Todo
+from User import User
 
 app = FastAPI() #crée l'application
 
 todos: list[Todo] = []
+users: list[User] = []
 
+# --------------Users --------------------
+@app.get("/users")
+def get_users() -> list[User]:
+    return users
+
+@app.post("/users")
+def create_user(user: User) -> User:
+    user.id = len(users) + 1
+    users.append(user)
+    return user
+
+#================== Todos ======================
 @app.get("/todos")
 def get_todos() -> list[Todo]:
     return todos
 
 @app.post("/todos")
 def create_todo(todo: Todo) -> Todo:
-    todo.id = len(todos) + 1
-    todos.append(todo)
-    return todo
+    for user in users:
+        if user.id == todo.user_id:
+            todo.id = len(todos) + 1
+            todos.append(todo)
+            return todo
+
+    raise HTTPException(status_code=404, detail="Users pas trouvé")
 
 @app.get("/todos/{id}")
 def get_todo(id: int) -> Todo:
